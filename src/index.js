f7881a4d3c012ac051552e5420505956084b8f39
sviatj <asviatsukha@gmail.com>
2023-09-17 22:34:39 +0300

    feat: complete task'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function reverse (n) {
-  
+
+n = Math.abs(n);
+
+if (n < 0) {
+  let string = n.toString();
+  let arr = string.split('');
+  let reverse = arr.reverse('');
+  let result = reverse.join('');
+
+  return result;
+}
+else {
+  let string = n.toString();
+  let arr = string.split('');
+  let reverse = arr.reverse('');
+  let result = reverse.join('');
+
+  return result;
+}
+
 }
